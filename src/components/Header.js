0e6814499a51fea34c2bb12c3cebea83c4e8f131
max-mcgee-valtech <max.mcgee@valtech.com>
2022-03-16 16:40:04 -0700

    updates packagin

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,56 @@ import "./Header.css";
 import Valtech from "../images/original.png";
 import USA from "../images/usa.png";
 import { ShoppingCartOutlined, MenuOutlined } from "@ant-design/icons";
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
 
 const { Search } = Input;
 const categories = ["Shirts", "Pants", "Shoes"];
 
 const Header = () => {
   const { authenticate, account } = useMoralis();
+
+  const handleConnectToWallet = () => {
+    // Create a connector
+    const connector = new WalletConnect({
+      bridge: "https://bridge.walletconnect.org", // Required
+      qrcodeModal: QRCodeModal,
+    });
+
+    // Check if connection is already established
+    if (!connector.connected) {
+      // create new session
+      connector.createSession();
+    }
+
+    // Subscribe to connection events
+    connector.on("connect", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      // Get provided accounts and chainId
+      const { accounts, chainId } = payload.params[0];
+    });
+
+    connector.on("session_update", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      // Get updated accounts and chainId
+      const { accounts, chainId } = payload.params[0];
+    });
+
+    connector.on("disconnect", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      // Delete connector
+    });
+  };
+
   return (
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
@@ -29,7 +73,7 @@ const Header = () => {
               className="login"
               key="1"
               type="primary"
-              onClick={() => authenticate()}
+              onClick={() => handleConnectToWallet()}
             >
               {account ? (
                 <span>{account.slice(0, 5)}...</span>
