459c21a94070f7db305e5e0106a08131f61a03c2
cvols-valtech <chris.volonnino@gmail.com>
2022-05-11 10:57:01 -0400

    feat: add new landing page

---
+++ src/hooks/useOnScreen.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export default function useIntersection(element) {
+  const [isVisible, setState] = useState(false);
+
+  console.log({ element });
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setState(entry.isIntersecting);
+      },
+      { rootMargin: '0px', threshold: '0.75' }
+    );
+
+    element.current && observer.observe(element.current);
+
+    return () => observer.unobserve(element.current);
+  }, [element]);
+
+  return isVisible;
+}
